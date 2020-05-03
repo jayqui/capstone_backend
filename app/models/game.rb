ee699b7662ceef0ce204c40d060ca4ec92c5788e
ernestomelchor <nurturelove88@gmail.com>
2020-05-03 13:45:43 -0500

    add ability to see a friendly version of the date and time a game is scheduled

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,4 +7,8 @@ class Game < ApplicationRecord
   belongs_to :field
   belongs_to :player
   has_many :players_attending, class_name: "PlayerGame"
+
+  def friendly_date_time!
+    date_time.strftime("%A, %b %d, %Y at:%l:%M %p")
+  end
 end
