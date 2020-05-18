e8259992711ea0ead924e6aff87110222c77280b
ernestomelchor <nurturelove88@gmail.com>
2020-05-18 15:32:33 -0500

    add validations to field model so that the input fields are not empty

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,4 +1,9 @@
 class Field < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :open_time, presence: true
+  validates :close_time, presence: true
+  validates :image_url, presence: true
 
   # associations
   has_many :games
